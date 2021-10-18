71a112a151c93d5b4b7d109fbda578e1d6f8ce27
AlessioMoggio <alessio.moggio@gmail.com>
2021-10-18 22:46:52 +0200

    [fix] Added extendedGCD explanation and script

--- General/Mathematics/Extended_GCD.py
+++ General/Mathematics/Extended_GCD.py
@@ -1,12 +1,12 @@
-def extendedGCD(p, q):
+def extendedGCD(p, q, loop):
     if (p == 0):
         return 0, 1
-
-    x, y = extendedGCD(p % q, q)
-
-    x2 = y - (p // q) * x
+    print('loop:', loop, ' p:', p, ' q:', q)
+    x, y = extendedGCD(q % p, p, loop+1)
+    print('loop:', loop,' x:', x, ' y:', y)
+    x2 = y - (q // p) * x
     y2 = x
 
     return x2, y2
 
-print(extendedGCD(26513, 32321))
+print(extendedGCD(26513, 32321, 1))
